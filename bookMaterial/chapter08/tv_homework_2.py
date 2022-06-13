b68d79a8d3342b274fecc538b48b00dd0fb55dc6
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-06-13 20:15:50 +0500

    fix homework_2

--- bookMaterial/chapter08/tv_homework_2.py
+++ bookMaterial/chapter08/tv_homework_2.py
@@ -1,55 +1,79 @@
-class tv(object):
-    a = None
+class TV():
+
+    MAX_VOLUME = 14
+    MAX_CHANNEL = 100
+    MIN_CHANNEL = 1
 
     def __init__(self, volume = 7, chanel = 1):
         self.volume = volume
         self.chanel = chanel
+        self.save_volume = 0
+    
+    def press_button(self, choice):
+        choice = choice.lower()
+        try:
+            self.channel_jump(int(choice))
+        except:
+            if choice == '+':
+                self.volume_level(1)
+            elif choice == '-':
+                self.volume_level(-1)
+            elif choice == 'm':
+                self.mute()
+            elif choice == 'n':
+                self.chanels_change(1)
+            elif choice == 'p':
+                self.chanels_change(-1)
     
     def volume_level(self, a):
         self.volume += a
         if self.volume < 0:
             self.volume = 0
-        elif self.volume > 14:
-            self.volume = 14
-        if self.volume == 0:
+        elif self.volume > self.MAX_VOLUME:
+            self.volume = self.MAX_VOLUME
+        print(f'\n\nSet volume: {self.volume}\n\n')      
+
+    def mute(self):
+        if self.volume != 0:
+            self.save_volume = self.volume 
+            self.volume = 0
             print('\n\nMute\n\n')
         else:
+            self.volume = self.save_volume
             print(f'\n\nSet volume: {self.volume}\n\n')
-
+        
     def chanels_change(self, a):
         self.chanel += a
-        if self.chanel > 100:
+        if self.chanel > self.MAX_CHANNEL:
             self.chanel = a
-        elif self.chanel < 1:
-            self.chanel = 100
+        elif self.chanel < self.MIN_CHANNEL:
+            self.chanel = self.MAX_CHANNEL
         print(f'\n\nSet channel: {self.chanel}\n\n')
 
     def channel_jump(self, a):
-        if 1 < a < 100:
+        if self.MIN_CHANNEL < a < self.MAX_CHANNEL:
             self.chanel = a
             print(f'\n\nSet channel: {self.chanel}\n\n')
         else:
             print(f'\n\nOut of channels range. You still watching channel number - {self.chanel}!\n\n')
 
 def main():
-    tv_control = tv()
-    print(f"\nI'm turn on!\nChannel - {tv_control.chanel}\nVolume set - {tv_control.volume}\n")
+    tv_control = TV()
+    print(f"\nI'm turn on!\nChannel - {tv_control.chanel}\nVolume set - {tv_control.volume}")
     choice = None 
     while choice != 'Off':
-        choice = input('TV menu:\nN : next channel\nP : previous channel\n+ : turn up the volume\n- : turn down the volume\nM : mute\nNumber : chosse chanel (1 - 100)\nTurn off - Off\n\nYour choice:\t').lower()
-        try:
-            tv_control.channel_jump(int(choice))
-        except:
-            if choice == '+':
-                tv_control.volume_level(1)
-            elif choice == '-':
-                tv_control.volume_level(-1)
-            elif choice == 'm':
-                tv_control.volume_level(-tv_control.volume)
-            elif choice == 'n':
-                tv_control.chanels_change(1)
-            elif choice == 'p':
-                tv_control.chanels_change(-1)
+        print('''
+TV menu:
+N : next channel
+P : previous channel
++ : turn up the volume
+- : turn down the volume
+M : mute
+Number : chosse chanel (1 - 100)
+Turn off - Off
+''')
+        choice = input('Your choice: ')
+        tv_control.press_button(choice)
     else:
         print("\n\nGood bye! I'm off!\n\n")
 
