5a7e3f08b9de2d05fa228c78cc21cb701869403a
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-08-02 00:13:52 +0500

    add functions damage_in & damage_out for character

--- homework_chapter_9/LoJ/character.py
+++ homework_chapter_9/LoJ/character.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import randrange, triangular, sample
 
 class CharacterPlayer():
 
@@ -18,16 +18,54 @@ class CharacterPlayer():
         self.armor += armor_upgrade
         self.health += health_upgrade
 
+    def damage_out(self):
+        random_rate = triangular(0.5, 2)
+        level_rate = self.level * triangular(0.1, 1)
+        damage_out = self.strength * random_rate + level_rate
+        damage_out = int(damage_out)
+        return damage_out
+
+    def damage_in(self, enemie_damage):
+        armor_rate = self.armor * triangular(0.6, 1)
+        armor_rate = int(armor_rate)
+        damage_in = enemie_damage * armor_rate
+
+
 
         
-vova = Character_Player()
-print(vova.level)
-print(vova.strength)
+vova = CharacterPlayer()
+print(f'My level - {vova.level}')
+print(f'My Strenght - {vova.strength}')
+print(f'My Health - {vova.health}')
+i = 0
+while i < 20:
+    i +=1
+    vova.damage_out()
 print('--------')
 vova.level_up()
 print('--------')
-print(vova.level)
-print(vova.strength)
+print(f'My level - {vova.level}')
+print(f'My Strenght - {vova.strength}')
+print(f'My Health - {vova.health}')
+i = 0
+while i < 20:
+    i +=1
+    vova.damage_out()
+
+print('--------')
+print('--------')
+print('--------')
+print('--------')
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+print(sample(['red', 'blue'], counts=[4, 2], k=5))
+
+
 
 
 
