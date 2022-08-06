57a87b85206ad3144555ce1755825c2370ce0b12
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-08-06 23:09:01 +0500

    add damages

--- homework_chapter_9/LoJ/enemies.py
+++ homework_chapter_9/LoJ/enemies.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import randrange, triangular
 
 class Enemie():
     def __init__(self, multiplier = 1):
@@ -7,5 +7,18 @@ class Enemie():
         self.strength = randrange(1,4) * multiplier
         self.armor = randrange(1,4) * multiplier
         self.health = randrange(10,15) * multiplier
+
+    def damage_out(self):
+        random_rate = triangular(0.5, 2)
+        level_rate = self.level * triangular(0.1, 1)
+        damage_out = self.strength * random_rate + level_rate
+        damage_out = int(damage_out)
+        return damage_out
+
+    def damage_in(self, enemie_damage):
+        armor_rate = self.armor * triangular(0.6, 1)
+        armor_rate = int(armor_rate)
+        damage_in = enemie_damage * armor_rate
+        return damage_in
  
 
