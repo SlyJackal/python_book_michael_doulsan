a798f6f828c1569f80caa4dd719a8cee8d8863f4
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-08-27 20:22:52 +0500

    prepare and clean code for main.py

--- homework_chapter_9/LoJ/character.py
+++ homework_chapter_9/LoJ/character.py
@@ -32,49 +32,6 @@ class CharacterPlayer():
         damage_in = enemie_damage * armor_rate
         return damage_in
 
-
-
-        
-vova = CharacterPlayer()
-print(f'My level - {vova.level}')
-print(f'My Strenght - {vova.strength}')
-print(f'My Health - {vova.health}')
-i = 0
-while i < 20:
-    i +=1
-    vova.damage_out()
-print('--------')
-vova.level_up()
-print('--------')
-print(f'My level - {vova.level}')
-print(f'My Strenght - {vova.strength}')
-print(f'My Health - {vova.health}')
-i = 0
-while i < 20:
-    i +=1
-    vova.damage_out()
-
-print('--------')
-print('--------')
-print('--------')
-print('--------')
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-print(sample(['red', 'blue'], counts=[4, 2], k=5))
-
-
-
-
-
-
-
-
-
-#if name == "main":
-#    print("This is a module for character.")
-#    input("\n\nPress the enter key to exit.")
+if name == "main":
+    print("This is a module for character.")
+    input("\n\nPress the enter key to exit.")
--- homework_chapter_9/LoJ/world.py
+++ homework_chapter_9/LoJ/world.py
@@ -4,11 +4,11 @@ class World():
     def __init__(self):
         map_size = randrange(10, 20)
         print(f'Map Size - {map_size}')
-        world = list()
+        map = list()
         for i in range(map_size):
-            world.append(i)
-        print(world)
+            map.append(i)
+        print(map)
 
-
-hell = World()
-
+if name == "main":
+    print("This is a module for world creation.")
+    input("\n\nPress the enter key to exit.")
