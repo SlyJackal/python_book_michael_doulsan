c5601c0ef17d5acfca67d44175cad4319580777e
Vladimir Chernyshev <slyjackal4@gmail.com>
2022-08-27 20:45:21 +0500

    delete * in self.level

--- homework_chapter_9/LoJ/enemies.py
+++ homework_chapter_9/LoJ/enemies.py
@@ -3,7 +3,7 @@ from random import randrange, triangular
 class Enemie():
     def __init__(self, multiplier = 1):
         self.multiplier = multiplier
-        self.level *= multiplier
+        self.level = multiplier
         self.strength = randrange(1,4) * multiplier
         self.armor = randrange(1,4) * multiplier
         self.health = randrange(10,15) * multiplier
